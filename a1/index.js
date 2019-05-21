47eaf878e6fcaed242d6fa817f20960309fac04c
Kaitian Xie <xkaitian@gmail.com>
2019-05-20 17:31:42 -0700

    A1: Load initial messages from stringyfied JSON object

--- a1/index.js
+++ a1/index.js
@@ -2,6 +2,15 @@ var itemInput = document.getElementById("new-item");
 var addButton = document.getElementById("add-btn");
 var clearButton = document.getElementById("clear-btn");
 var myList = document.getElementById("my-list");
+var initialMessages = `[{"message":"item 1"},{"message":"item 2"}]`;
+
+var initialMessagesArray = JSON.parse(initialMessages);
+for (var i = 0; i < initialMessagesArray.length; i++) {
+  var listItem = document.createElement("li");
+  listItem.innerText = initialMessagesArray[i].message;
+  listItem.className = "list-item";
+  myList.appendChild(listItem);
+}
 
 function addNewItem() {
   var listItem = document.createElement("li");
