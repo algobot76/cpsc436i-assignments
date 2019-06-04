d65dedb0d72ac4a85454c70b4a1ba4451ac29076
Kaitian Xie <xkaitian@gmail.com>
2019-06-04 02:21:34 -0600

    A2: Use Popup in Item

--- a2/src/components/Item.js
+++ a2/src/components/Item.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import Popup from './Popup';
 import PropTypes from 'prop-types';
 
-const Item = ({ onClick, completed, text }) => (
-  <li
-    onClick={onClick}
-    style={{
-      textDecoration: completed ? 'line-through' : 'none'
-    }}
-  >
-    {text}
-  </li>
-);
+class Item extends React.Component {
+  state = {
+    showPopup: false
+  };
+
+  closePopup = () => {
+    this.setState({
+      showPopup: false
+    });
+  };
+
+  openPopup = () => {
+    console.log('fuck');
+    this.setState({
+      showPopup: true
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <li
+            style={{
+              textDecoration: this.props.completed ? 'line-through' : 'none'
+            }}
+          >
+            {this.props.text}
+          </li>
+          <button type="button" onClick={this.props.onClick}>
+            {this.props.completed ? 'restore' : 'delete'}
+          </button>
+          <button type="button" onClick={this.openPopup}>Popup</button>
+        </div>
+        <div>
+          {this.state.showPopup ? (
+            <Popup onClick={this.closePopup} text={this.props.text} />
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
 
 Item.propTypes = {
   onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
+  showPopup: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 };
 
