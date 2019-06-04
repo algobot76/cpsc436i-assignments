9acb66e4442201a5f5f4ee7219549f01d49a9b56
Kaitian Xie <xkaitian@gmail.com>
2019-06-04 10:35:57 -0700

    A2: Regorg

--- a2/src/components/VisibleItemList.js
+++ a2/src/components/VisibleItemList.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { toggleItem } from '../actions';
-import ItemList from '../components/ItemList';
+import ItemList from './ItemList';
 import { VisibilityFilters } from '../actions';
 
 const getVisibleItems = (items, filter) => {
