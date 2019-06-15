8e0e14a581494fafdf8c047274dfe99179fc827b
Kaitian Xie <xkaitian@gmail.com>
2019-06-15 12:05:52 -0700

    A3: Small fixes

--- a3/util/db.js
+++ a3/util/db.js
@@ -1,63 +1,76 @@
 class Database {
-  #id = 0;
-  #db = [];
 
   constructor(initialData) {
+    this._id = 0;
+    this._db = [];
     if (initialData.length > 0) {
       initialData.forEach(entry => {
         const obj = {
-          id: #id,
+          id: this._id,
           data: entry,
         };
-        #db.push(obj);
-        #id++;
+        this._db.push(obj);
+        this._id++;
       });
     }
   }
 
   add(entry) {
     const obj = {
-      id: #id,
+      id: this._id,
       data: entry,
     };
-    #id++;
-    #db.push(obj);
+    this._id++;
+    this._db.push(obj);
   }
 
   delete(id) {
-    const idx = #search(id);
+    const idx = this._search(id);
     if (idx !== -1) {
-      #db.splice(idx, 1);
+      this._db.splice(idx, 1);
     }
   }
 
   clearAll() {
-    #id = 0;
-    #db = [];
+    this._id = 0;
+    this._db = [];
   }
 
-  #search(id) {
-    if (#db.length === 0) {
+  query(id) {
+    const idx = this._search(id);
+    if (idx !== -1) {
+      return this._db[idx];
+    } else {
+      return null;
+    }
+  }
+
+  getAll() {
+    return this._db;
+  }
+
+  _search(id) {
+    if (this._db.length === 0) {
       return -1;
     }
 
     let start = 0;
-    let end = #db.length - 1;
+    let end = this._db.length - 1;
     while (start + 1 < end) {
       let mid = (start + end) / 2;
-      if (#db[mid].id === id) {
+      if (this._db[mid].id === id) {
         return mid;
-      } else if (#db[mid].id < mid) {
+      } else if (this._db[mid].id < mid) {
         start = mid;
       } else {
         end = mid;
       }
     }
 
-    if (#db[start].id === id) {
+    if (this._db[start].id === id) {
       return start;
     }
-    if (#db[end].id === id) {
+    if (this._db[end].id === id) {
       return end;
     }
 
