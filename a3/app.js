ef75ca97c5b50255c34ab29912d091805e7a743c
Kaitian Xie <xkaitian@gmail.com>
2019-06-15 17:05:43 -0700

    A3: Change app setup

--- a3/app.js
+++ a3/app.js
@@ -9,9 +9,6 @@ const messagesRouter = require('./routes/messages');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
