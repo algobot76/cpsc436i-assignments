4d0786fa6642736c8ab2b5d8865c170ffaaeffcf
Kaitian Xie <xkaitian@gmail.com>
2019-06-16 15:00:49 -0700

    A3: Integerate with reudx devtools

--- a3/client/src/index.js
+++ a3/client/src/index.js
@@ -3,10 +3,21 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 render(
   <Provider store={store}>
