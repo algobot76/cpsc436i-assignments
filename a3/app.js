7e41fae0eadf64e4b32fdc8fc391c6260aa7d2dc
Kaitian Xie <xkaitian@gmail.com>
2019-06-15 17:02:25 -0700

    A3: Update app.js

--- a3/app.js
+++ a3/app.js
@@ -14,9 +14,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client', 'build', 'index.html')));
 
 app.use('/messages', messagesRouter);
 
