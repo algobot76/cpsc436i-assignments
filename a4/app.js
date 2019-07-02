85786fd808d18e0f865255166e7b8dae064536a4
Kaitian Xie <xkaitian@gmail.com>
2019-07-01 22:34:51 -0700

    A4: Connect to mongodb

--- a4/app.js
+++ a4/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const messagesRouter = require('./routes/messages');
 
 const app = express();
 
+// connect to mongodb
+mongoose
+  .connect('mongodb://localhost:27017/a4', { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
 // view engine setup
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/messages', messagesRouter);
